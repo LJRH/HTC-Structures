e063d6cee7b0bdfad7619d1c9a8686cd3d100f83
Luke Higgins <pmljrh@leeds.ac.uk>
2019-04-11 17:20:43 +0100

    updates bpath orca script to include bpath_HMF03

--- geom_opt_bpath.sh
+++ geom_opt_bpath.sh
@@ -8,3 +8,4 @@
 module add orca
 orca /nobackup/pmljrh/data/HTC-structures/bpath_HMF01/bpath_HMF01.inp > /nobackup/pmljrh/data/HTC-structures/bpath_HMF01/geom_opt01/bpath_HMF01.out
 orca /nobackup/pmljrh/data/HTC-structures/bpath_HMF02/bpath_HMF02.inp > /nobackup/pmljrh/data/HTC-structures/bpath_HMF02/geom_opt01/bpath_HMF02.out
+orca /nobackup/pmljrh/data/HTC-structures/bpath_HMF03/bpath_HMF03.inp > /nobackup/pmljrh/data/HTC-structures/bpath_HMF03/geom_opt01/bpath_HMF03.out
